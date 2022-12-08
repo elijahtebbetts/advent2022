523fb64955ebd564d9a2458446826e3b137a62d6
Elijah Tebbetts <elijahtebbetts@gmail.com>
2022-12-08 17:04:25 -0500

    Complete solution 1, part 2

--- 01/main.ts
+++ 01/main.ts
@@ -3,19 +3,33 @@ import { openInputFile } from '../common'
 function solution(): number {
     const data = openInputFile(__dirname + '/input.txt').split('\n');
     let runningTotal: number = 0;
-    let maximum: number = 0;
+    let first:number = 0;
+    let second:number = 0;
+    let third:number = 0;
 
     for (let entry of data) {
         if (entry !== '') {
             runningTotal += parseInt(entry);
         }
         else {
-            if (runningTotal > maximum) maximum = runningTotal;
+            if (runningTotal > first) {
+                third = second;
+                second = first;
+                first = runningTotal;
+            } 
+            else if (runningTotal > second) {
+                third = second;
+                second = runningTotal;
+            }
+            else if (runningTotal > third) {
+                third = runningTotal;
+            }
+            
             runningTotal = 0;
         }
     }
 
-    return maximum;
+    return first + second + third;
 }
 
 console.log(solution());
