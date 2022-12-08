ded7e811b6242febf6dae02ea51341f2a7696890
Elijah Tebbetts <elijahtebbetts@gmail.com>
2022-12-08 16:55:33 -0500

    Complete solution 1, part 1

--- 01/main.ts
+++ 01/main.ts
@@ -1,3 +1,21 @@
 import { openInputFile } from '../common'
 
-console.log(openInputFile(__dirname + '/input.txt'));
+function solution(): number {
+    const data = openInputFile(__dirname + '/input.txt').split('\n');
+    let runningTotal: number = 0;
+    let maximum: number = 0;
+
+    for (let entry of data) {
+        if (entry !== '') {
+            runningTotal += parseInt(entry);
+        }
+        else {
+            if (runningTotal > maximum) maximum = runningTotal;
+            runningTotal = 0;
+        }
+    }
+
+    return maximum;
+}
+
+console.log(solution());
