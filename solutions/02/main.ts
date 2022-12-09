34f03fc535c148d80a6777c195928a5e30d19f3c
Elijah Tebbetts <elijahtebbetts@gmail.com>
2022-12-09 15:33:54 -0500

    [02] Complete day 2, part 1
    Added function to calculate the point value for a given round.
    
    Solution loops through data and
    prints/returns the sum of each
    round's point value.

--- solutions/02/main.ts
+++ solutions/02/main.ts
@@ -1,18 +1,18 @@
 import { openInputFile } from '../../common'
 
-function solution() {
-    class Move {
-        name: string;
-        points: number;
-        defeats: Move | null;
+class Move {
+    name: string;
+    points: number;
+    defeats: Move | null;
 
-        constructor(name: string, points: number, defeats?: Move) {
-            this.name = name;
-            this.points = points;
-            this.defeats = defeats || null;
-        }
+    constructor(name: string, points: number, defeats?: Move) {
+        this.name = name;
+        this.points = points;
+        this.defeats = defeats || null;
     }
+}
 
+function solution(): number {
     let rock = new Move("Rock", 1);
     let paper = new Move("Paper", 2);
     let scissors = new Move("Scissors", 3);
@@ -26,8 +26,37 @@ function solution() {
         X: rock, Y: paper, Z: scissors
     };
 
-
     const data = openInputFile(__dirname + '/input.txt');
+    const ROW_LENGTH = 4; //Each row of data input is 4 characters long
+    let pointsTotal: number = 0;
+
+    for (let i = 0; i < data.length; i += ROW_LENGTH) {
+        const enemyMoveAlias = data[i];
+        const playerMoveAlias = data[i+2];
+        const enemyMove = moveAliases[enemyMoveAlias];
+        const playerMove = moveAliases[playerMoveAlias];
+
+        pointsTotal += calculateRoundPoints(enemyMove, playerMove);
+    }
+
+    console.log(`Total points: ${pointsTotal}`);
+    return pointsTotal;
+}
+
+function calculateRoundPoints(enemyMove: Move, playerMove: Move): number {
+    const DRAW_VALUE = 3;
+    const WIN_VALUE = 6;
+
+    let points: number = playerMove.points;
+
+    if (playerMove.defeats === enemyMove) {
+        points += WIN_VALUE;
+    }
+    else if (playerMove === enemyMove) {
+        points += DRAW_VALUE;
+    }
+
+    return points;
 }
 
 solution();
