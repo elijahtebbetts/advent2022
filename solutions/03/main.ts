48b4421c74f2bd9ad8ba79a3186b6ed5e6dae0e9
Elijah Tebbetts <elijahtebbetts@gmail.com>
2022-12-09 16:52:27 -0500

    [03] Complete day 3, part 1
    Report a sum of the priorities of each common item.

--- solutions/03/main.ts
+++ solutions/03/main.ts
@@ -3,12 +3,30 @@ import { openInputFile } from '../../common'
 function solution() {
     const data = openInputFile(__dirname + '/input.txt').split('\n');
 
+    let prioritySum = 0;
+
     for (let sack of data) {
-        const firstHalf = sack.slice(sack.length / 2);
+        const firstHalf = sack.slice(0, sack.length / 2);
         const secondHalf = sack.slice(sack.length / 2, sack.length);
-        
-        console.log(findCommonItem(firstHalf, secondHalf));
+        const commonItem = findCommonItem(firstHalf, secondHalf);
+
+        //prioritySum += calculatePriority(commonItem);
+        prioritySum += calculatePriority(commonItem);
     }
+
+    console.log(prioritySum);
+}
+
+function calculatePriority(item: string): number {
+    const UPPERCASE_LIMIT = 90;
+    const UPPERCASE_OFFSET = 64;
+    const LOWERCASE_OFFSET = 96;
+    const charCode = item.charCodeAt(0);
+    
+    if (charCode <= UPPERCASE_LIMIT) 
+        return charCode - UPPERCASE_OFFSET + 26;
+    else 
+        return charCode - LOWERCASE_OFFSET;
 }
 
 function findCommonItem(a: string, b: string): string {
