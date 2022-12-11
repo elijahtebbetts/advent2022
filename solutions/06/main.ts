2665095f63ad305424568a0c6f4d829f1b7b77b0
Elijah Tebbetts <elijahtebbetts@gmail.com>
2022-12-10 21:05:08 -0500

    [06] Complete day 6, part 1

---
+++ solutions/06/main.ts
@@ -0,0 +1,53 @@
+import { openInputFile } from '../../common'
+
+function solution() {
+    const data = openInputFile(__dirname + '/input.txt').split('');
+    const BUFFER_LENGTH = 4;
+    let buffer: string[] = [];
+
+    //Fill our buffer with the first n characters
+    for (let i = 0; i < BUFFER_LENGTH; i++) {
+        buffer.push(data.shift() as string)
+    }
+
+    function incrementBuffer() {
+        buffer.shift();
+        buffer.push(data.shift() as string);
+    }
+
+    function bufferIsUnique() {
+        let set = new Set();
+        
+        for (let char of buffer) {
+            set.add(char);
+        }
+
+        return set.size === BUFFER_LENGTH;
+    }
+
+    let startFound:boolean = false;
+    let numProcessed = BUFFER_LENGTH;
+
+    while(data.length > 0) {
+        console.log(buffer);
+        if (bufferIsUnique()) {
+            startFound = true;
+            break;
+        }
+        else {
+            numProcessed++;
+            incrementBuffer();
+        }
+    }
+
+    if (startFound) {
+        console.log(numProcessed);
+    }
+    else {
+        console.log("Unique buffer not found");
+    }
+
+    
+}
+
+solution();
