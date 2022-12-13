722fe7fff0a6294e6fc6122a689ab4bdd7675560
Elijah Tebbetts <elijahtebbetts@gmail.com>
2022-12-13 14:02:46 -0500

    [08] Code consolidation
    View distance and visibility are now calculated at the same time.

--- solutions/08/main.ts
+++ solutions/08/main.ts
@@ -4,49 +4,61 @@ const data = openInputFile(__dirname + '/input.txt').split('\n').map((line) => l
 const HEIGHT = data.length;
 const WIDTH = data[0].length;
 
-type ViewDistance = {
-    right: number,
-    left: number,
-    up: number,
-    down: number
-}
-
 function solution() {    
     console.log(`Grid is ${WIDTH} x ${HEIGHT}`);
-    
-    //Part 1 answer
-    console.log(`Trees visible from edge: ${getNumVisible()}`);
 
-    //Part 2 answer
-    console.log(`Highest scenic score: ${getMaxScenicScore()}`);
-}
+    let visibleCount = 0;
+    let highestScenicScore = 0;
 
-function getNumVisible() {
-    //All trees along the edge are visible
-    let visibleCount: number = (WIDTH * 2) + (HEIGHT * 2) - 4;
-    console.log(`${visibleCount} edge trees`);
+    for (let x = 0; x < WIDTH; x++) {
+        for (let y = 0; y < HEIGHT; y++) {
+            const viewDistance: ViewDistance = getViewDistance(x, y);
+            if (viewDistance.visible) visibleCount++;
 
-    //Skip edge trees in our check
-    for (let x = 1; x < WIDTH - 1; x++) {
-        for (let y = 1; y < HEIGHT - 1; y++) {
-            const visible = isVisible(x, y);
-            if (visible) visibleCount++;
+            const scenicScore = getScenicScore(viewDistance);
+            if (scenicScore > highestScenicScore) highestScenicScore = scenicScore;
         }
     }
+    
+    //Part 1 answer
+    console.log(`Trees visible from edge: ${visibleCount}`);
 
-    return visibleCount;
+    //Part 2 answer
+    console.log(`Highest scenic score: ${highestScenicScore}`);
 }
 
 function getTreeHeight(x: number, y: number) {
     return data[y][x];
 }
 
+type ViewDistance = {
+    right: number,
+    left: number,
+    up: number,
+    down: number,
+    visible?: boolean //true if tree is visible from outside any edge
+}
+
+type Visibility = {
+    right: boolean,
+    left: boolean,
+    up: boolean,
+    down: boolean
+}
+
 function getViewDistance(x: number, y: number): ViewDistance {
-    let viewDistance: Record<'right'|'left'|'up'|'down', number> = {
+    let viewDistance: ViewDistance = {
         right: 0,
         left:  0,
         up:    0,
         down:  0
+    }
+
+    let visibility: Visibility = {
+        right: true,
+        left: true,
+        up: true,
+        down: true
     }
 
     const maxHeight = getTreeHeight(x, y);
@@ -54,100 +66,52 @@ function getViewDistance(x: number, y: number): ViewDistance {
     //Right
     for (let checkX = x + 1; checkX < WIDTH; checkX++) {
         viewDistance.right++;
-        if (getTreeHeight(checkX, y) >= maxHeight) break;
+        if (getTreeHeight(checkX, y) >= maxHeight) {
+            visibility.right = false;
+            break;
+        }
     }
 
     //Left
     for (let checkX = x - 1; checkX >= 0; checkX--) {
         viewDistance.left++;
-        if (getTreeHeight(checkX, y) >= maxHeight) break;
+        if (getTreeHeight(checkX, y) >= maxHeight) {
+            visibility.left = false;
+            break;
+        }
     }
 
     //Up
     for (let checkY = y - 1; checkY >= 0; checkY--) {
         viewDistance.up++;
-        if (getTreeHeight(x, checkY) >= maxHeight) break;            
+        if (getTreeHeight(x, checkY) >= maxHeight) {
+            visibility.up = false;
+            break;
+        }            
     }
 
     //Down
     for (let checkY = y + 1; checkY < HEIGHT; checkY++) {
         viewDistance.down++;
-        if (getTreeHeight(x, checkY) >= maxHeight) break;
+        if (getTreeHeight(x, checkY) >= maxHeight) {
+            visibility.down = false;
+            break;
+        }
     }
+
+    viewDistance.visible = visibility.right ||
+        visibility.left ||
+        visibility.up ||
+        visibility.down
 
     return viewDistance;
 }
 
-function getScenicScore(x: number, y: number): number {
-    const viewDistance: ViewDistance = getViewDistance(x, y);
-
+function getScenicScore(viewDistance: ViewDistance): number {
     return viewDistance.right * 
         viewDistance.left * 
         viewDistance.up * 
         viewDistance.down;
-}
-
-function getMaxScenicScore(): number {
-    let maxScore: number = 0;
-
-    for (let x = 0; x < WIDTH - 1; x++) {
-        for (let y = 0; y < HEIGHT - 1; y++) {
-            const score = getScenicScore(x, y);
-            if (score > maxScore) maxScore = score;
-        }
-    }
-
-    return maxScore;
-}
-
-function isVisible(x:number, y:number): boolean {
-    const height = data[y][x];
-    let visible = true;
-    
-    //Right
-    for (let checkX = x+1; checkX < WIDTH; checkX++) {
-        if (data[y][checkX] >= height) {
-            visible = false;
-            break;
-        }
-    }
-
-    if (visible) return true;
-    visible = true;
-
-    //Left
-    for (let checkX = x-1; checkX >= 0; checkX--) {
-        if (data[y][checkX] >= height) {
-            visible = false;
-            break;
-        }
-    }
-
-    if (visible) return true;
-    visible = true;
-
-    //Up
-    for (let checkY = y - 1; checkY >= 0; checkY--) {
-        if (data[checkY][x] >= height) {
-            visible = false;
-            break;
-        }
-    }
-
-    if (visible) return true;
-    visible = true;
-
-    //Down
-    for (let checkY = y + 1; checkY < HEIGHT; checkY++) {
-        if (data[checkY][x] >= height) {
-            visible = false;
-            break;
-        }
-    }
-
-    if (visible) return true;
-    
-    return false;
 }
 
 solution();
